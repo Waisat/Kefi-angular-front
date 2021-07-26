6f761fb3c7e1767546445ff091e99cc806049b1c
Waisat <jeanlouis.gallouin@gmail.com>
2021-07-26 23:18:45 +0200

    hhl

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -45,7 +45,7 @@ export class LoginPageComponent implements OnInit {
       ).subscribe(user => {
         this.userArray.push(user)
         userLoginInfo = JSON.parse(this.userArray)
-        this.cookieService.put(userLoginInfo.name, userLoginInfo.jwt, {domain:".herokuapp.com",  sameSite:'none', secure:true, expires: new Date(Date.now() + 100000*8)})
+        this.cookieService.put(userLoginInfo.name, userLoginInfo.jwt, {domain:"radiant-crag-76180.herokuapp.com",  sameSite:'none', secure:true, expires: new Date(Date.now() + 100000*8)})
         const token = this.user.getCookieJwt("kefi_token")
         if(token){
           this.IsWait = false
