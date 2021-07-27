6a4801b9807e955f2481766eee07b1692a215ffa
Waisat <jeanlouis.gallouin@gmail.com>
2021-07-27 16:20:53 +0200

    new

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -48,7 +48,7 @@ export class LoginPageComponent implements OnInit {
         let date = new Date()
         let timer = new Date(Date.now() +  3600000 )
 
-        this.cookieService.put(userLoginInfo.name, userLoginInfo.jwt, {domain:"kefiassociation.fr", expires:(timer.toString()), secure:false})
+        this.cookieService.put(userLoginInfo.name, userLoginInfo.jwt, {domain:"kefiassociation.fr", expires:(timer.toString()), secure:true})
         const token = this.user.getCookieJwt("kefi_token")
         if(token){
           this.IsWait = false
