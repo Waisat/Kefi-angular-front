ee91c88d47d5fdf90fd8ca8ba65c4abd31b94052
Waisat <jeanlouis.gallouin@gmail.com>
2021-08-14 18:16:30 +0200

    updatehttps

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -182,7 +182,7 @@ export class UserService {
 
   sendPhotoUser(photoUser: FormData): Observable<FormData> {
 
-    return this.http.post<FormData>(this.ConfigUrl + "/send_user_photo", photoUser, this.optionFile)
+    return this.http.post<FormData>(this.ConfigUrl + "/send_user_photo", photoUser, this.options)
       .pipe(
         catchError(this.handleError)
       );
